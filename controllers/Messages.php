8d5469847d2475ef502a4d985a263b7fbbf27cde
DiamondSystems <me@diamondsystems.org>
2021-10-13 15:04:00 +0500

    #fix - filter status

--- controllers/Messages.php
+++ controllers/Messages.php
@@ -4,6 +4,9 @@ namespace DS\Feedback\Controllers;
 
 use Backend\Classes\Controller;
 use BackendMenu;
+use Backend\Classes\Controller;
+use Illuminate\Support\Facades\DB;
+use DS\Feedback\Models\FeedbackStatus;
 
 class Messages extends Controller
 {
@@ -27,7 +30,21 @@ class Messages extends Controller
 
     public function listExtendQuery($query)
     {
-        $query->leftJoin('ds_feedback_status', 'ds_feedback_messages.status_id', '=', 'ds_feedback_status.id')
-            ->addSelect('ds_feedback_status.is_hide_message as is_hide_message');
+        # filter - status
+        # ------------------
+        $messagesTable = $query->getQuery()->from;
+        $statusTable   = (new FeedbackStatus())->getTable();
+
+        if (empty($query->getQuery()->columns))
+            $query->addSelect(DB::raw($messagesTable.'.*'));
+
+        $query->leftJoin($statusTable, $messagesTable.'.status_id', '=', 'ds_feedback_status.id')
+            ->addSelect(DB::raw($statusTable.'.is_hide_message as is_hide_message'));
+
+        foreach ($query->getQuery()->wheres as &$where)
+        {
+            if (isset($where['column']) && strripos($where['column'], '.') === false)
+                $where['column'] = $messagesTable.'.'.$where['column'];
+        }
     }
 }
