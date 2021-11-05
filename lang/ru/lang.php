9a3d2adba2b1a2d942ab065255684441a305d31f
DiamondSystems <me@diamondsystems.org>
2021-11-05 11:50:13 +0500

    RU language

---
+++ lang/ru/lang.php
@@ -0,0 +1,23 @@
+<?php return [
+    'feedback' => [
+        'category' => [
+            'uncategorized' => '--- Без категории ---',
+        ],
+        'subject' => [
+            'custom_subject' => '--- Произвольская тема ---',
+        ],
+        'message_list' => [
+            'filter' => [
+                'status'            => 'Статус',
+                'subject'           => 'Тема',
+                'category'          => 'Категория',
+                'is_hide_message'   => 'Сообщения со статусом "закрыто"',
+                'is_uncategorized'  => 'Без категории',
+                'is_custom_subject' => 'Произвольская тема',
+            ],
+            'columns' => [
+                'button' => 'Кнопка',
+            ],
+        ],
+    ],
+];
